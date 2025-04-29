80ac51df593ee3b97fd4302ef6a8d3329525c9a3
caminhosheitor <00001107322145sp@al.educacao.sp.gov.br>
2025-04-29 11:38:42 -0300

    nessa aula aprendemos a como fazer funções, de diferentes jeitos.

---
+++ function.py
@@ -0,0 +1,24 @@
+
+   
+
+   
+   
+
+def somarDoisNumeros(nota_01, nota_02):
+    nota_01 = nota_01 
+    nota_02 = nota_02 
+
+    resultado = nota_01 + nota_02
+
+    return  print(resultado)
+
+#somarDoisNumeros(17, 17)
+
+
+A_01 = 70
+B_02 = 2
+ 
+
+resultado = A_01 * B_02
+
+somarDoisNumeros (70, 2)
